bfb8654c454bfb3289b5f6b33e6dafb28da8cc3a
Abdullah-Rehman786 <Air711@live.com>
2022-03-17 09:21:49 -0500

    complete 10.2.2 query params

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,22 +2,30 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     // get /hello
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello, Spring";
-    }
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring";
+//    }
 
     // get /gb
     @GetMapping("gb")
     @ResponseBody
     public String gb() {
         return "Gb, Spring";
+    }
+
+    //handles request of form /hello?name=LaunchCode
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
 }
