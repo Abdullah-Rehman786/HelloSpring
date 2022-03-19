1f64f030f6cd5ee9621a24bd6fa015254f05f00d
Abdullah-Rehman786 <Air711@live.com>
2022-03-19 10:59:59 -0500

    complete 13.5 thymeleaf templates up to conditionals

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,10 +1,13 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@ResponseBody
 @RequestMapping("hello")
 public class HelloController {
 
@@ -15,6 +18,7 @@ public class HelloController {
 //    }
 
     // get /hello/gb
+    @ResponseBody
     @GetMapping("gb")
     public String gb() {
         return "Gb, Spring";
@@ -22,26 +26,62 @@ public class HelloController {
 
     //handles request of form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String greeting = "Hello " + name + "!";
+
+        model.addAttribute("greeting",greeting);
+        return "hello";
     }
 
     //handles /hello/LaunchCode
     @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        String greeting = "Hello " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     // /hello/form
     @GetMapping("form")
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='POST'>" +
-                "<input type='text' name='name' />" +
-                "<input type='submit' value='Greet me!' />" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
+    }
+
+    //handles request of form /hello/multilingual?name=LaunchCode&language=French
+//    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="multilingual")
+//    @ResponseBody
+//    public String helloDiffLanguages(@RequestParam String name, @RequestParam String language) {
+//
+//        return HelloController.createMessage(name, language);
+//    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+
+        names.add("LaunchCode");
+        names.add("Abdullah");
+        names.add("Chris");
+
+        model.addAttribute("names", names);
+
+        return "hello-list";
+    }
+
+    public static String createMessage(String name, String language) {
+        String greeting = "";
+        if(language.equals("English")){
+            greeting = "Hello ";
+        } else if (language.equals("French")){
+            greeting = "Bonjour ";
+        } else if (language.equals("Spanish")){
+            greeting = "Hola ";
+        } else if (language.equals("German")){
+            greeting = "Hallo ";
+        } else if (language.equals("Italian")){
+            greeting = "Ciao ";
+        }
+
+        return greeting + name + "!";
     }
 }
